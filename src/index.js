ac67584bf7945000d88500fcc5d1a60acf9b3d6a
Rachel Johnson <Rachel.Johnson.2000@live.com>
2025-10-02 14:26:26 -0500

    Update to not have hash routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Experience from "./pages/Experience";
